2d96a9e4ec63648c9e8d87b56cea29eb754f0cc1
936968629 <936968629@qq.com>
2017-11-23 13:08:33 +0800

    web
    edit project

--- README.md
+++
@@ -1,2 +0,0 @@
-# VueShop
-django+Vue
--- VueShop/urls.py
+++ VueShop/urls.py
@@ -1,4 +1,4 @@
-"""shop URL Configuration
+"""VueShop URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
